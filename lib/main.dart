bb02b00626be680f866568d62abb2d4084447f7c
Dheeraj <ds1887030@gmail.com>
2022-05-12 04:48:28 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'LoginScreen.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    home: LoginScreen(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
